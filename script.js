ed1556909b5bc43004c4512a6a0c7e27876b7189
mashefat <mashefat7373@gmail.com>
2021-09-03 01:41:12 +0600

    added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const searchBook = () =>{
     
 
     // load data 
-    const url =  `http://openlibrary.org/search.json?q=${searchText}`;
+    const url =  `https://openlibrary.org/search.json?q=${searchText}`;
     fetch (url)
     .then(res => res.json())
     .then(data => displaySearch(data.docs))
